d3b8b7c2dba66a186c7188b0d4929178e60d7ded
Kwak <ehdgusjdbswl@gmail.com>
2023-07-19 21:09:26 +0900

    feat - Member객체와 joinColumn 설정

--- src/main/java/com/example/board/board/domain/Board.java
+++ src/main/java/com/example/board/board/domain/Board.java
@@ -1,14 +1,12 @@
 package com.example.board.board.domain;
 
+import com.example.board.member.domain.Member;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Access;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -21,20 +19,28 @@ public class Board {
     private Long boardId;
 
     //Todo : JoinColumn 확실히 짚고가기.
-//    @JoinColumn(name = "member_id")
-//    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "member_id", referencedColumnName = "memberId")
+//    private Long memberId;          //이렇게 바로 받아오면 안되고안되고 Member 객체를 받아와야 하는가?
+                                        //멤버 객체 만을 받아오는것만으로 column 키설정이 가능?
+    private Member member;
 
+    private String writer;
+
+    @NotNull
     private String title;
 
+    @NotNull
     private String content;
 
     @Column(name = "create_date_time")
     private String createDateTime;
 
     @Builder
-    public Board(Long boardId, String title, String content, String createDateTime){
+    public Board(Long boardId,Member member, String title, String content, String createDateTime){
         this.boardId = boardId;
-//        this.memberId = memberId;
+        this.member = member;
+        this.writer = member.getNickname();
         this.title = title;
         this.content = content;
         this.createDateTime = createDateTime;
