422f36c18aac17c426a942e24fe6e2cd2955243e
Kwak <ehdgusjdbswl@gmail.com>
2023-07-21 02:51:39 +0900

    test - memberService 테스트 개발
    -findMemberById()
    -findMemberByNickname()

--- src/test/java/com/example/board/member/MemberServiceTest.java
+++ src/test/java/com/example/board/member/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.example.board.member;
 
 import com.example.board.member.domain.Member;
 import com.example.board.member.dto.requestDto.MemberRequest;
+import com.example.board.member.dto.responseDto.MemberResponse;
 import com.example.board.member.repository.MemberRepository;
 import com.example.board.member.service.MemberService;
 import org.junit.jupiter.api.Assertions;
@@ -10,13 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 //@Transactional //Test코드에 붙으면 임시공간 생겨서 종료 후 롤백
@@ -43,22 +46,67 @@ public class MemberServiceTest {
                 .nickname("석")
                 .build();
 
-        Mockito.when(memberRepository.save(any(Member.class))).thenReturn(mockMember);
+        when(memberRepository.save(any(Member.class))).thenReturn(mockMember);
 
         memberService.saveMemberProfile(memberRequest);
 
-        Mockito.verify(memberRepository, Mockito.times(1)).save(any(Member.class));
+        verify(memberRepository, times(1)).save(any(Member.class));
     }
 
     @DisplayName("id로 회원을 찾는다.")
     @Test
     void findMemberById(){
 
+        Long memberId = 1L;
+
+        MemberResponse memberResponse = MemberResponse.builder()
+                .memberId(memberId)
+                .email("테스팅")
+                .nickname("동")
+                .build();
+
+        Member mockMember = Member.builder()
+                .memberId(memberId)
+                .email("테스팅")
+                .nickname("동")
+                .build();
+
+        // Mock (memberRepository) 객체의 동작 정의
+        when(memberRepository.findByMemberId(memberId)).thenReturn(Optional.of(mockMember));
+
+       // 테스트하려는 메소드를 호출
+        MemberResponse result = memberService.findMemberById(memberId);
+//
+//        // 예상한 동작을 검증
+//        assertEquals(memberId, result.getMemberId());
+//        assertEquals("테스팅", result.getEmail());
+//        assertEquals("동", result.getNickname());
+
+        // memberRepository의 findByMemberId 메소드가 올바른 파라미터로 호출되었는지 검증합니다.
+        verify(memberRepository, times(1)).findByMemberId(memberId);
     }
 
     @DisplayName("닉네임으로 회원을 찾는다")
     @Test
     void findMemberByNickname(){
+        String memberNickname = "동";
 
+        MemberResponse memberResponse = MemberResponse.builder()
+                .memberId(1L)
+                .email("테스팅")
+                .nickname("동")
+                .build();
+
+        Member mockMember = Member.builder()
+                .memberId(1L)
+                .email("테스팅")
+                .nickname("동")
+                .build();
+
+        when(memberRepository.findByNickname(memberNickname)).thenReturn(Optional.of(mockMember));
+
+        memberService.findMemberByNickname(memberNickname);
+
+        verify(memberRepository,times(1)).findByNickname(memberNickname);
     }
 }
