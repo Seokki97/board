fb9611895a74b498b18b017f465f2b450d5b01fc
Kwak <ehdgusjdbswl@gmail.com>
2023-08-04 21:22:51 +0900

    test - 조회기능 테스트 코드 작성

--- src/test/java/com/example/board/board/service/read/BoardReadTest.java
+++ src/test/java/com/example/board/board/service/read/BoardReadTest.java
@@ -1,101 +1,34 @@
-package com.example.board.board;
+package com.example.board.board.service.read;
 
 import com.example.board.board.domain.Board;
-import com.example.board.board.dto.requestDto.BoardRequest;
+import com.example.board.board.exception.BoardNotFoundException;
 import com.example.board.board.repository.BoardRepository;
 import com.example.board.board.service.BoardService;
 import com.example.board.member.domain.Member;
-import com.example.board.member.dto.responseDto.MemberResponse;
 import com.example.board.member.exception.MemberNotFoundException;
-import com.example.board.member.repository.MemberRepository;
-import com.example.board.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import javax.swing.text.html.Option;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.util.AssertionErrors.assertNotNull;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
-public class BoardServiceTest {
+public class BoardReadTest {
 
     @InjectMocks
     private BoardService boardService;
 
     @Mock
     private BoardRepository boardRepository;
-
-    @Mock
-    private MemberService memberService;
-
-    @Mock
-    private MemberRepository memberRepository;
-
-
-    @DisplayName("글 작성 테스트 - 회원아이디 존재")
-    @Test
-    //회원이 발견되지 않는 경우 (주어진 ID로 회원을 찾을 수 없는 경우)와 같은 다양한 시나리오를 커버하도록 추가적인 테스트 케이스를 고려해보세요.
-    public void writePostTest(){
-        Long memberId = 1L;
-
-        MemberResponse mockMemberResponse = MemberResponse.builder()
-                .memberId(memberId)
-                .email("테스트")
-                .nickname("동")
-                .build();
-
-        BoardRequest boardRequest = BoardRequest.builder()
-                .title("제목스껄")
-                .content("내용스껄")
-                .build();
-
-        Board mockBoard = Board.builder()
-                .member(mockMemberResponse.toMember())
-                .boardId(123L)
-                .title(boardRequest.getTitle())
-                .content(boardRequest.getContent())
-                .build();
-
-        // < 회원아이디 존재할 때 >
-        when(memberService.findMemberById(memberId)).thenReturn(mockMemberResponse);
-
-        when(boardRepository.save(any(Board.class))).thenReturn(mockBoard);
-
-        boardService.writePost(boardRequest,memberId);
-
-        verify(boardRepository,times(1)).save(any(Board.class));
-
-    }
-
-    @DisplayName("글 작성 테스트(예외) - 회원아이디 미존재")
-    @Test
-    void writePostTestException(){
-        Long memberId = 1L;
-
-        BoardRequest boardRequest = BoardRequest.builder()
-                .title("제목스껄")
-                .content("내용스껄")
-                .build();
-
-        // < 회원아이디 존재하지 않을 때 >
-        when(memberService.findMemberById(memberId)).thenThrow(MemberNotFoundException.class);
-
-        // 메서드 호출 시 예외가 발생하는지 검증 (MemberNotFoundException이 발생해야 함)
-        assertThrows(MemberNotFoundException.class, () -> boardService.writePost(boardRequest, memberId));
-
-        // boardRepository.save가 호출되지 않아야 함
-        verify(boardRepository, never()).save(any(Board.class));
-    }
 
     @DisplayName("게시물 ID 로 단일조회")
     @Test
@@ -113,8 +46,6 @@ public class BoardServiceTest {
                 .boardId(boardId)
                 .title("제목")
                 .content("내용")
-                .createDateTime(LocalDateTime.now())
-                .updateDateTime(LocalDateTime.now())
                 .build();
 
         when(boardRepository.findById(boardId)).thenReturn(Optional.of(mockBoard));
@@ -176,7 +107,6 @@ public class BoardServiceTest {
         Board board3 = mock(Board.class);
 
 
-
 //        List<Board> matchingBoardList = new ArrayList<>();
 //        matchingBoardList.add(board2);
 //        matchingBoardList.add(board3);
@@ -201,15 +131,31 @@ public class BoardServiceTest {
         assertEquals(2,result.size());
 
     }
-}
 
-/*
+    @DisplayName("조회 예외발생-> 게시판 ID 없음")
+    @Test
+    void BoardIdExceptionTest(){
+        Long boardId = 1L;
 
-    // 회원 ID 로 해당 ID 게시물 전체 조회
-    public List<Board> showAllPostByMemberId(Long id){
-        List<Board> boardList = boardRepository.findAllByMemberId(id);
-        return boardList;
+        when(boardRepository.findById(boardId)).thenThrow(BoardNotFoundException.class);
+
+        assertThrows(BoardNotFoundException.class, () -> boardService.showPostById(boardId));
+    }
+
+    @DisplayName("조회 예외발생-> 회원 ID 없음")
+    @Test
+    void MemberIdExceptionTest(){
+        Long exceptId = 1L;
+
+        //회원 아이디를 조회 했을때 반환되는 값이 하나도 없음.
+        when(boardRepository.findAllByMemberId(exceptId)).thenReturn(Collections.emptyList());
+
+        //예외 발생
+        assertThrows(MemberNotFoundException.class, () -> boardService.showAllPostByMemberId(exceptId));
+
+        verify(boardRepository,times(1)).findAllByMemberId(exceptId);
     }
 
 
- */
+}
+
