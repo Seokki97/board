d993d1e157589a15757821df3e59bbe97dd68042
Kwak <ehdgusjdbswl@gmail.com>
2023-08-05 18:40:59 +0900

    test - 게시판 조회 테스트 (미완료)

--- src/test/java/com/example/board/board/service/read/BoardReadTest.java
+++ src/test/java/com/example/board/board/service/read/BoardReadTest.java
@@ -5,6 +5,7 @@ import com.example.board.board.exception.BoardNotFoundException;
 import com.example.board.board.repository.BoardRepository;
 import com.example.board.board.service.BoardService;
 import com.example.board.member.domain.Member;
+import com.example.board.member.dto.responseDto.MemberResponse;
 import com.example.board.member.exception.MemberNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -88,49 +89,57 @@ public class BoardReadTest {
         //Todo : 총 3개의 게시글 중에서 memberId가 일치하는 2개의 게시물을 찾아야 하는데... 어떻게 짜지?
     void showAllPostByMemberIdTest(){
         Long memberId1 = 1L;
+        Member member1 = mock(Member.class);
+        when(member1.getMemberId()).thenReturn(memberId1);       //멤버1 주입
+
         Long memberId2 = 2L;
+        Member member2 = mock(Member.class);
+        when(member2.getMemberId()).thenReturn(memberId2);
 
-        Member member1 = Member.builder()
-                .memberId(memberId1)
-                .email("동")
-                .nickname("동동")
-                .build();
+        Board board1= mock(Board.class);
+        when(board1.getBoardId()).thenReturn(1L);
+        Board board2= mock(Board.class);
+        when(board2.getBoardId()).thenReturn(2L);
+        Board board3= mock(Board.class);
+        when(board3.getBoardId()).thenReturn(3L);
+                                                            //board boardId memberId
+        when(board1.getMember()).thenReturn(member1);       //1     1       1
+        when(board2.getMember()).thenReturn(member2);       //2     2       2
+        when(board3.getMember()).thenReturn(member1);       //3     3       1
 
-        Member member2 = Member.builder()
-                .memberId(memberId2)
-                .email("구리")
-                .nickname("구리구리")
-                .build();
+        assertEquals(1L,board1.getBoardId());
+        assertEquals(2L,board2.getBoardId());
+        assertEquals(3L,board3.getBoardId());
+        assertEquals(1L,board1.getMember().getMemberId());
+        assertEquals(2L,board2.getMember().getMemberId());
+        assertEquals(1L,board3.getMember().getMemberId());
 
-        Board board1 = mock(Board.class);
-        Board board2 = mock(Board.class);
-        Board board3 = mock(Board.class);
+        List<Board> mockListAll = new ArrayList<>();
+        mockListAll.add(board1); mockListAll.add(board2); mockListAll.add(board3);
 
+        List<Board> matchingList = new ArrayList<>();
+        matchingList.add(board1); matchingList.add(board3);
 
-//        List<Board> matchingBoardList = new ArrayList<>();
-//        matchingBoardList.add(board2);
-//        matchingBoardList.add(board3);
+//        when(boardRepository.findAll()).thenReturn(mockListAll);
+        when(boardRepository.findAllByMemberId(1L)).thenReturn(mockListAll);
 
-        //원하는 값을 주입가능.
-        when(board1.getMember()).thenReturn(member1);
-        when(board2.getMember()).thenReturn(member2);
-        when(board3.getMember()).thenReturn(member2);
+        List<Board> result = boardService.showAllPostByMemberId(1L);
 
-        List<Board> listAll = new ArrayList<>();
-        listAll.add(board1);
-        listAll.add(board2);
-        listAll.add(board3);
-
-        when(boardRepository.findAll()).thenReturn(listAll);
-//        when(boardRepository.findAllByMemberId(memberId2)).thenReturn(matchingBoardList);
-
-        List<Board> result = boardService.showAllPostByMemberId(memberId2);
-
-        verify(boardRepository,times(1)).findAllByMemberId(memberId2);
+        verify(boardRepository,times(1)).findAllByMemberId(1L);
 
         assertEquals(2,result.size());
 
+
     }
+
+//    public List<Board> showAllPostByMemberId(Long id) throws MemberNotFoundException{
+//        List<Board> boardList = boardRepository.findAllByMemberId(id);
+//        if(boardList.size()==0){
+//            throw new MemberNotFoundException();
+//        }
+//        return boardList;
+//    }
+
 
     @DisplayName("조회 예외발생-> 게시판 ID 없음")
     @Test
